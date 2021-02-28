437f6689a3ac088d2b8b6f181fc508548a6eb16c
Nathália Caetano <cath.ztk27@gmail.com>
2021-02-28 18:32:55 -0300

    Add yup validation on nps-controller

--- src/controllers/NpsController.ts
+++ src/controllers/NpsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { getCustomRepository, Not, IsNull } from "typeorm";
 import { SurveysUsersRepository } from "../Repositories/SurveysUsersRepository";
+import * as yup from "yup";
 
 class NpsController {
 
@@ -15,37 +16,45 @@ class NpsController {
      */
 
     async execute(request: Request, response: Response) {
-        const { survey_id } = request.params;
+        try {
+            const schema = yup.object().shape({
+                survey_id: yup.string().uuid().required()
+            })
 
-        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+            const { survey_id } = await schema.validate(request.body)
 
-        const surveysUsers = await surveysUsersRepository.find({
-            survey_id,
-            value: Not(IsNull()),
-        })
-        const detractors = surveysUsers.filter(
-            (survey) => survey.value >= 0 && survey.value <= 6
-        ).length;
-        const promoters = surveysUsers.filter(
-            (survey) => survey.value >= 9 && survey.value <= 10
-        ).length;
-        const passives = surveysUsers.filter(
-            (survey) => survey.value >= 7 && survey.value <= 8
-        ).length;
+            const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-        const totalAnswers = surveysUsers.length;
+            const surveysUsers = await surveysUsersRepository.find({
+                survey_id,
+                value: Not(IsNull()),
+            })
+            const detractors = surveysUsers.filter(
+                (survey) => survey.value >= 0 && survey.value <= 6
+            ).length;
+            const promoters = surveysUsers.filter(
+                (survey) => survey.value >= 9 && survey.value <= 10
+            ).length;
+            const passives = surveysUsers.filter(
+                (survey) => survey.value >= 7 && survey.value <= 8
+            ).length;
 
-        const calculate = Number(
-            (((promoters - detractors) / totalAnswers) * 100).toFixed(2)
-        );
+            const totalAnswers = surveysUsers.length;
 
-        return response.json({
-            detractors,
-            promoters,
-            passives,
-            totalAnswers,
-            nps: calculate,
-        })
+            const calculate = Number(
+                (((promoters - detractors) / totalAnswers) * 100).toFixed(2)
+            );
+
+            return response.json({
+                detractors,
+                promoters,
+                passives,
+                totalAnswers,
+                nps: calculate,
+            })
+        } catch (err) {
+            return response.status(400).json({ error: err.message })
+        }
     }
 }
 
